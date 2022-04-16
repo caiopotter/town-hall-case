e53c8b864937ab14b1217b9f149a4e8aa843995e
caiopotter <limapotter@gmail.com>
2022-04-16 02:50:13 -0300

    School list view and school card component

---
+++ src/components/SchoolCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container>
+        <v-card outlined>
+            <v-card-title>
+                Escola {{index}}
+            </v-card-title>
+            <v-card-subtitle>
+                Alguma outra coisa
+            </v-card-subtitle>
+            <v-card-text>
+                Nada aqui ainda
+            </v-card-text>
+            <v-card-actions>
+                    <v-col cols="12" class="mt-n6 text-right">
+                        <v-btn outlined @click="selectSchool">
+                            <v-icon>mdi-plus</v-icon>
+                            Detalhes
+                            </v-btn>
+                    </v-col>
+                </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'SchoolCard',
+  props: ['index', 'schoolClass'],
+
+  methods: {
+        selectSchool(){
+            this.$emit('selectedClass', this.schoolClass);
+        }
+    }
+}
+</script>
---
+++ src/views/SchoolList.vue
@@ -0,0 +1,40 @@
+<template>
+    <v-container>
+        <v-card flat>
+    <v-toolbar
+      dense
+      elevation="0"
+    >
+      <v-text-field
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+        :clearable="true"
+        placeholder="Pesquisar escola"
+      ></v-text-field>
+    </v-toolbar>
+  </v-card>
+        <v-row>
+            <v-col class="mb-n6" cols="12" sm="12" md="6" lg="4" v-for="(n, index) in 10" :key="index">
+                    <school-card :index="n" @selectedClass="log"></school-card>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+
+<script>
+import SchoolCard from '../components/SchoolCard.vue';
+
+export default {
+  name: 'SchoolList',
+  components: {
+    SchoolCard
+  },
+
+  methods: {
+        log(){
+            console.log('click');
+        }
+    }
+}
+</script>
