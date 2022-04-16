5703c60bc1947b02ce9668803fe235ea316b9914
caiopotter <limapotter@gmail.com>
2022-04-16 02:51:06 -0300

    Class list view

---
+++ src/views/ClassList.vue
@@ -0,0 +1,22 @@
+<template>
+    <v-card>
+        
+    </v-card>
+</template>
+
+<script>
+export default {
+    name: 'SchoolCard',
+    props: ['index'],
+
+    methods: {
+        log(){
+            console.log('click');
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
