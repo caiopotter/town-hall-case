1ea73b5431044bf11bb7128239bf5756375a6830
caiopotter <limapotter@gmail.com>
2022-04-19 01:24:05 -0300

    vuetify theme

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,25 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    icons: {
+        iconfont: 'mdi',
+    },
+    theme: {
+        options: {
+            customProperties: true,
+         },
+        themes: {
+          light: {
+              blue: '#008FBE',
+              light_blue: '#70CAF0',
+              very_light_blue: '#008DB5',
+              lightest_blue: '#DBEAEF',
+              dark_blue: '#004A80',
+              navy_blue: '#1E4260',
+              grey: '#ECECEC',
+              blue_grey: '#9AA9B5',
+              dark_grey: '#808080'
+          }
+        }
+    }
 });
