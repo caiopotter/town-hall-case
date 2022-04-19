649d1221c915949aacc8c534b29c91380d6ef95e
caiopotter <limapotter@gmail.com>
2022-04-19 01:37:30 -0300

    School and classes cards

---
+++ src/components/Cards/ClassCard.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-container>
+        <v-card outlined color="lightest_blue">
+            <v-card-title>
+                {{schoolClass.name}}
+            </v-card-title>
+            <v-card-subtitle>
+                {{schoolClass.year}} ano
+            </v-card-subtitle>
+            <v-card-text>
+                Turno: {{schoolClass.shift}}
+            </v-card-text>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'ClassCard',
+  props: ['index', 'schoolClass'],
+
+  methods: {
+        
+    }
+}
+</script>
---
+++ src/components/Cards/SchoolCard.vue
@@ -0,0 +1,37 @@
+<template>
+    <v-container>
+        <v-card outlined color="lightest_blue">
+            <v-card-title>
+                {{school.name}}
+            </v-card-title>
+            <v-card-subtitle>
+                {{school.address}}
+            </v-card-subtitle>
+            <v-card-text>
+                Zona {{school.zone}}
+            </v-card-text>
+            <v-card-actions>
+                    <v-col cols="12" class="mt-n6 text-right">
+                        <v-btn outlined @click="selectSchool">
+                            <v-icon>mdi-plus</v-icon>
+                            Detalhes
+                            </v-btn>
+                    </v-col>
+                </v-card-actions>
+        </v-card>
+    </v-container>
+</template>
+
+<script>
+
+export default {
+  name: 'SchoolCard',
+  props: ['index', 'school'],
+
+  methods: {
+        selectSchool(){
+            this.$emit('selectedClass', {...this.school, ...{id: this.index}});
+        }
+    }
+}
+</script>
