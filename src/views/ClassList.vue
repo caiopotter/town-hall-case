a8dc2850df676a03f5234686fc007b45b79ad28d
caiopotter <limapotter@gmail.com>
2022-04-19 01:37:12 -0300

    Create school and classes

--- src/views/ClassList.vue
+++ src/views/ClassList.vue
@@ -1,6 +1,6 @@
 <template>
     <v-container>
-        <v-card>
+        <v-card color='light_blue'>
             <v-card-title>
                 {{selectedSchool.name}}
             </v-card-title>
@@ -13,17 +13,101 @@
         </v-card>
         <v-row>
             <v-col class="mt-4" cols="12">
-                <v-btn outlined color="forest"><v-icon class="mr-2">mdi-plus</v-icon>Adicionar turma</v-btn>
+                <v-btn @click="newClassDialog = true" outlined color="navy_blue"><v-icon class="mr-2">mdi-plus</v-icon>Adicionar turma</v-btn>
             </v-col>
             <v-col class="mb-n6" cols="12" sm="12" md="6" lg="4" v-for="(schoolClass, index) in selectedSchoolClasses" :key="index">
                     <class-card :index="index" :schoolClass="schoolClass"></class-card>
             </v-col>
         </v-row>
+
+        <v-dialog v-model="newClassDialog" max-width="500px">
+        <v-card>
+          <v-card-title class="mt-n1" :style="{'background-color': 'var(--v-' + 'blue' + '-base)'}">
+            <span :style="{'color': 'var(--v-' + 'grey' + '-base)'}">
+                Nova Turma
+            </span>
+            <v-spacer></v-spacer>
+            <v-icon @click="closeDialog" style="color:white">mdi-close</v-icon>
+          </v-card-title>
+        <v-card-text class="mt-4">
+            Preencha todos os campos para cadastrar uma nova turma:
+        </v-card-text>
+        <v-card-actions>
+
+          <v-col
+            class="mx-5"
+            lg="10"
+            md="10"
+            sm="10"
+            xs="12"
+          >
+        <v-form v-model="isValidForm">
+          <div>
+            <v-text-field
+              dense
+              v-model="nameValue"
+              label="Nome da turma"
+              outlined
+              :rules="[rules.required, rules.min]"
+            ></v-text-field>
+          </div>
+          <div>
+              <v-select
+              :items="yearOptions"
+              v-model="yearValue"
+              label="Ano"
+              dense
+              outlined
+              :rules="[rules.required]"
+            ></v-select>
+          </div>
+          <div>
+              <v-select
+              :items="shiftOptions"
+              v-model="shiftValue"
+              label="Turno"
+              dense
+              outlined
+              :rules="[rules.required]"
+            ></v-select>
+          </div>
+     
+          <div>
+            <v-spacer></v-spacer>
+            <v-btn :loading='loading' :disabled="!isValidForm" color="blue" class="white--text"
+            @click="saveFields">Cadastro
+            </v-btn>
+          </div>
+        </v-form>
+      </v-col>
+        </v-card-actions>
+    </v-card>
+    </v-dialog>
+    
+    <v-snackbar
+      top
+      v-model="userNotification"
+      timeout="5000"
+      :color="statusColor"
+    >
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="userNotification = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
     </v-container>
 </template>
 
 <script>
-import ClassCard from '../components/ClassCard.vue';
+import ClassCard from '../components/Cards/ClassCard.vue';
 
 export default { 
     name: 'ClassList',
@@ -40,8 +124,66 @@ export default {
         }
     },
     data: () => ({
-        
+        newClassDialog: false,
+        yearValue: '',
+        nameValue: '',
+        shiftValue: '',
+        isValidForm: false,
+        userNotification: false,
+        loading: false,
+        yearOptions: ['6º', '7º', '8º', '9º'],
+        shiftOptions: ['Manhã', 'Tarde', 'Noite', 'Integral'],
+        text: 'oi?',
+        statusColor: '',
+        rules: {
+        required: value => !!value || 'Campo obrigatório.',
+        min: v => v.length >= 3 || 'Ao menos 3 caracteres',
+        }
     }),
+    methods:{
+        selectClass(selectedSchool){
+            this.$store.dispatch('getSchoolClassesFromServer', selectedSchool).then(isOk => {
+            if(isOk){
+                this.$router.push('/classList')
+            }
+            })
+        },
+        async saveFields(){
+            if(this.isValidForm){
+                this.loading = true;
+                this.$store.dispatch('createClass', 
+                {
+                    year: this.yearValue, 
+                    name: this.nameValue, 
+                    shift: this.shiftValue,
+                    schoolId: this.selectedSchool.id
+                }).then(response => {this.reportResultToUser(response)})
+            }
+        },
+        clearSignUpForm(){
+            this.emailValue = '';
+            this.nameValue = '';
+            this.addressValue = '';
+            this.statusMessage = '';
+        },
+        closeDialog(){
+            this.clearSignUpForm();
+            this.newClassDialog = false;
+        },
+        reportResultToUser(response){
+            if(response.success){
+                this.text = "Sucesso ao criar Escola";
+                this.statusColor = "green"
+            }else{
+                this.text = "Ocorreu algum erro ao criar Escola";
+                this.statusColor = "red"
+            }
+            this.userNotification = true;
+            this.loading = false;  
+            this.closeDialog();    
+        },
+        
+    }
 }
 </script>
 
--- src/views/SchoolList.vue
+++ src/views/SchoolList.vue
@@ -1,48 +1,187 @@
 <template>
     <v-container>
         <v-card flat>
-    <v-toolbar
-      dense
-      elevation="0"
-    >
-      <v-text-field
-        hide-details
-        prepend-icon="mdi-magnify"
-        single-line
-        :clearable="true"
-        placeholder="Pesquisar escola"
-      ></v-text-field>
-    </v-toolbar>
-  </v-card>
-        <v-row>
-            <v-col class="mb-n6" cols="12" sm="12" md="6" lg="4" v-for="(school, index) in schoolList" :key="index">
-                    <school-card :index="index" :school="school" @selectedClass="selectClass"></school-card>
+          <v-toolbar
+            dense
+            elevation="0"
+          >
+          </v-toolbar>
+          <v-row>
+            <v-col cols="12">
+              <v-btn outlined color="navy_blue" @click="newSchoolDialog = true"><v-icon class="mr-2">mdi-plus</v-icon>Adicionar Escola</v-btn>
             </v-col>
-        </v-row>
+            <v-col cols="12">
+              <v-text-field
+                hide-details
+                prepend-icon="mdi-magnify"
+                single-line
+                :clearable="true"
+                placeholder="Pesquisar escola"
+              ></v-text-field>
+            </v-col>
+          </v-row>
+        </v-card>
+      <v-row>
+          <v-col class="mb-n6" cols="12" sm="12" md="6" lg="4" v-for="(school, index) in schoolList" :key="index">
+                  <school-card :index="index" :school="school" @selectedClass="selectClass"></school-card>
+          </v-col>
+      </v-row>
+
+      <v-dialog v-model="newSchoolDialog" max-width="500px">
+        <v-card>
+          <v-card-title class="mt-n1" :style="{'background-color': 'var(--v-' + 'blue' + '-base)'}">
+            <span :style="{'color': 'var(--v-' + 'grey' + '-base)'}">
+                Nova Escola
+            </span>
+            <v-spacer></v-spacer>
+            <v-icon @click="closeDialog" style="color:white">mdi-close</v-icon>
+          </v-card-title>
+        <v-card-text class="mt-4">
+            Preencha todos os campos para cadastrar uma nova escola:
+        </v-card-text>
+        <v-card-actions>
+
+          <v-col
+            class="mx-5"
+            lg="10"
+            md="10"
+            sm="10"
+            xs="12"
+          >
+        <v-form v-model="isValidForm">
+          <div>
+            <v-text-field
+              dense
+              v-model="nameValue"
+              label="Nome da escola"
+              outlined
+              :rules="[rules.required, rules.min]"
+            ></v-text-field>
+          </div>
+          <div>
+            <v-text-field
+              dense
+              v-model="addressValue"
+              label="Endereço"
+              :rules="[rules.required, rules.min]"
+              outlined
+            ></v-text-field>
+          </div>
+          <div>
+            <v-select
+              :items="zoneOptions"
+              v-model="zoneValue"
+              label="Zona"
+              dense
+              outlined
+              :rules="[rules.required]"
+            ></v-select>
+          </div>
+     
+          <div>
+            <v-spacer></v-spacer>
+            <v-btn :loading='loading' :disabled="!isValidForm" color="blue" class="white--text"
+            @click="saveFields">Cadastro
+            </v-btn>
+          </div>
+        </v-form>
+      </v-col>
+        </v-card-actions>
+    </v-card>
+    </v-dialog>
+    
+    <v-snackbar
+      top
+      v-model="userNotification"
+      timeout="5000"
+      :color="statusColor"
+    >
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn
+          color="pink"
+          text
+          v-bind="attrs"
+          @click="userNotification = false"
+        >
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
     </v-container>
 </template>
 
 <script>
-import SchoolCard from '../components/SchoolCard.vue';
+import SchoolCard from '../components/Cards/SchoolCard.vue';
 
 export default {
   name: 'SchoolList',
   components: {
-    SchoolCard
+    SchoolCard,
   },
   computed:{
     schoolList(){
           return this.$store.getters.schoolList;
       },
   },
-  methods: {
-        selectClass(selectedSchool){
-            this.$store.dispatch('getSchoolClassesFromServer', selectedSchool).then(isOk => {
-              if(isOk){
-                this.$router.push('/classList')
-              }
-            })
-        }
+  data: () => ({
+    newSchoolDialog: false,
+    addressValue: '',
+    nameValue: '',
+    zoneValue: '',
+    zoneOptions: ['Norte', 'Sul', 'Leste', 'Oeste'],
+    isValidForm: false,
+    userNotification: false,
+    loading: false,
+    text: 'oi?',
+    statusColor: '',
+    rules: {
+      required: value => !!value || 'Campo obrigatório.',
+      min: v => v.length >= 3 || 'Ao menos 3 caracteres',
     }
+  }),
+  methods: {
+    selectClass(selectedSchool){
+        this.$store.dispatch('getSchoolClassesFromServer', selectedSchool).then(isOk => {
+          if(isOk){
+            this.$router.push('/classList')
+          }
+        })
+    },
+    async saveFields(){
+      if(this.isValidForm){
+        this.loading = true;
+        this.$store.dispatch('createSchool', 
+        {
+          address: this.addressValue, 
+          name: this.nameValue, 
+          zone: this.zoneValue, 
+        }).then(response => {this.reportResultToUser(response)})
+      }
+    },
+    clearSignUpForm(){
+      this.emailValue = '';
+      this.nameValue = '';
+      this.addressValue = '';
+      this.statusMessage = '';
+    },
+    closeDialog(){
+      this.clearSignUpForm();
+      this.newSchoolDialog = false;
+    },
+    reportResultToUser(response){
+      if(response.success){
+        this.text = "Sucesso ao criar Escola";
+        this.statusColor = "green"
+      }else{
+        this.text = "Ocorreu algum erro ao criar Escola";
+        this.statusColor = "red"
+      }
+      this.userNotification = true;
+      this.loading = false;  
+      this.closeDialog();    
+    },
+  },
 }
 </script>
