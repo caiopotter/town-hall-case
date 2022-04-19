7947ab8ea041c8ddce4baa7dad4e3c288f14fc7e
caiopotter <limapotter@gmail.com>
2022-04-19 01:37:43 -0300

    App bar

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,23 @@
       color="primary"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+    <div v-if="$route.path.includes('/classList')">
+      <v-row>
+        <v-col cols="3">
+        <div class="d-flex align-center">
+          <v-btn @click="$router.push('/')" large icon color="white">
+            <v-icon>mdi-arrow-left</v-icon>
+            </v-btn>
+        </div>
+        </v-col>
+        <v-col class="d-flex align-center" cols="9">
+          <div>Turmas - {{$store.getters.selectedSchool.name}}</div>
+        </v-col>
+      </v-row>
+    </div>
+    <span v-else>
+      Lista de Escolas
+    </span>
     </v-app-bar>
 
     <v-main>
@@ -53,6 +40,7 @@ export default {
   }),
   mounted(){
     this.$store.dispatch('getSchoolListFromServer')
+    this.$router.push('/').catch(() => {})
   },
 };
 </script>
